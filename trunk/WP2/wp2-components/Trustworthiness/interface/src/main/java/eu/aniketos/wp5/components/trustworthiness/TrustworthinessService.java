4bf85b52ad671f08a595b1a8679cd4380003bff3
ayautisukhin <ayautisukhin@78992972-45b9-6347-b9af-2376957681ad>
2011-06-30 13:36:31 +0000

    deleting wp2 folder for update

--- trunk/WP2/wp2-components/Trustworthiness/interface/src/main/java/eu/aniketos/wp5/components/trustworthiness/TrustworthinessService.java
+++
@@ -1,9 +0,0 @@
-package eu.aniketos.wp5.components.trustworthiness;
-
-
-public interface TrustworthinessService {
-
-    public double getTrustworthinessPrediction(String serviceInfo);
-	
-    public void receiveNotification(String notification);
-}
