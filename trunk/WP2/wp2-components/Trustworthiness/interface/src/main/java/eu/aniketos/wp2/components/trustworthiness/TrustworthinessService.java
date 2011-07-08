9b001d7150205d2bce0497fec2980720bf1626a7
ayautisukhin <ayautisukhin@78992972-45b9-6347-b9af-2376957681ad>
2011-07-08 09:25:21 +0000

    Trustworthiness component updated with 3 new interfaces

--- trunk/WP2/wp2-components/Trustworthiness/interface/src/main/java/eu/aniketos/wp2/components/trustworthiness/TrustworthinessService.java
+++ trunk/WP2/wp2-components/Trustworthiness/interface/src/main/java/eu/aniketos/wp2/components/trustworthiness/TrustworthinessService.java
@@ -1,9 +1,17 @@
 package eu.aniketos.wp2.components.trustworthiness;
 
+import java.util.List;
+
 
 public interface TrustworthinessService {
 
     public double getTrustworthinessPrediction(String serviceInfo);
 	
     public void receiveNotification(String notification);
+    
+    public void deriveMonitoringRules(String securityContract);
+    
+    public void receiveQoSUpdate(List<String> metrics);
+    
+    public void receiveReputationUpdate(double reputation);
 }
