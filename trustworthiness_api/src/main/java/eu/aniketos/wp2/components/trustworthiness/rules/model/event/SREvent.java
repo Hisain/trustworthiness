c1d8b4beae7aa76b6b1ca2c5a16659b4070f315e
Hisain Elshaafi <Hisain@users.noreply.github.com>
2012-11-15 13:11:30 +0000

    Delete event classes

--- trustworthiness_api/src/main/java/eu/aniketos/wp2/components/trustworthiness/rules/model/event/SREvent.java
+++
@@ -1,9 +0,0 @@
-package eu.aniketos.wp2.components.trustworthiness.rules.model.event;
-
-/**
- * @author Hisain Elshaafi (TSSG)
- * interface for trustworthiness rules events
- */
-public interface SREvent {
-
-}
